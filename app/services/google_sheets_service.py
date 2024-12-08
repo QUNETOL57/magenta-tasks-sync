bd4d40e9764d321b60d526a8e798688ed4052c99
qunetol <a.belolapotkov@automacon.ru>
2024-12-08 22:56:15 +0300

    Изменение задачи в google sheet

--- app/services/google_sheets_service.py
+++ app/services/google_sheets_service.py
@@ -1,6 +1,10 @@
+import re
+from typing import Optional
+
 import gspread
 from gspread.utils import ValueInputOption
 
+from app.helpers.list_helper import add_alphabet_keys
 from app.task_dto import TaskDTO
 from config import config
 
@@ -11,6 +15,7 @@ class GoogleSheetsService:
         self.sheet = gc.open_by_key(config.get('GOOGLE_SHEET_KEY'))
         self.worksheet = self.sheet.worksheet(config.get('GOOGLE_SHEET_WORKSHEET'))
 
+    # Поиск первой пустой строки
     def _find_first_empty_row(self) -> int:
         col_values = self.worksheet.col_values(1)
         for line_number, value in enumerate(col_values, start=1):
@@ -19,15 +24,45 @@ class GoogleSheetsService:
                 return line_number
         return len(col_values) + 1
 
+    # Поиск строки по префиксу (номеру) задачи
+    def _find_task_row_by_prefix(self, prefix: str) -> Optional[int]:
+        pattern = re.compile(f'{prefix}: .+', re.IGNORECASE)
+        cell = self.worksheet.find(pattern, in_column=1)
+        if cell:
+            return cell.row
+        return None
+
     def add_task(self, task: TaskDTO):
-        first_empty_row = self._find_first_empty_row()
-        hyperlink = f'=HYPERLINK("{task.url()}"; "{task.name()}")'
-        off_the_plan = bool(task.off_the_plan)
-        moving_on_to_the_next_sprint = bool(task.moving_on_to_the_next_sprint)
-        self.worksheet.update([[hyperlink, '', task.assignee, '', '', task.status, task.due_date, off_the_plan, '', '',
-                                moving_on_to_the_next_sprint]],
-                              f"A{first_empty_row}", value_input_option=ValueInputOption.user_entered)
-        # TODO реализовать получение столбца исполнителя
-        user_column = "Q"
-        self.worksheet.update([[task.sp_development]], f"{user_column}{first_empty_row}",
+        row = self._find_first_empty_row()
+        task_list = self.mapping(task)
+        self.worksheet.update([task_list], f"A{row}",
                               value_input_option=ValueInputOption.user_entered)
+
+    def update_task(self, task: TaskDTO):
+        row = self._find_task_row_by_prefix(task.key)
+        if row is not None:
+            old_task_list = self.worksheet.get(f"A{row}:AM{row}")[0]
+            task_list = self.mapping(task, old_task_list)
+            self.worksheet.update([task_list], f"A{row}",
+                                  value_input_option=ValueInputOption.user_entered)
+        else:
+            self.add_task(task)
+
+    @staticmethod
+    def mapping(task: TaskDTO, old_task_list=None):
+        if old_task_list is None:
+            old_task_list = ['' for _ in range(12)]
+        task_list = add_alphabet_keys(old_task_list)
+        task_list["A"] = task.hyperlink
+        task_list["B"] = task.assignee
+        task_list["C"] = task.type
+        task_list["D"] = task_list["D"]
+        task_list["E"] = task.status
+        task_list["F"] = task.stage_deadline
+        task_list["G"] = task.due_date
+        task_list["H"] = task.priority
+        task_list["I"] = task.off_the_plan
+        task_list["J"] = task.moving_next_sprint
+        task_list["L"] = task.sp_development
+
+        return list(task_list.values())
--- app/task_dto.py
+++ app/task_dto.py
@@ -2,17 +2,35 @@ class TaskDTO:
     def __init__(self, **kwargs):
         self.id = kwargs.get("id")
         self.key = kwargs.get("key")
-        self.summary = kwargs.get("summary")
+        self._summary = kwargs.get("summary")
+        self.stage_deadline = kwargs.get("stageDeadline")
+        self.type = kwargs.get("type")
         self.assignee = kwargs.get("assignee")
         self.status = kwargs.get("status")
         self.due_date = kwargs.get("dueDate")
-        self.off_the_plan = kwargs.get("offThePlan")
-        self.moving_on_to_the_next_sprint = kwargs.get("movingOnToTheNextSprint")
+        self._off_the_plan = kwargs.get("offThePlan")
+        self._moving_on_to_the_next_sprint = kwargs.get("movingOnToTheNextSprint")
         self.sp_development = kwargs.get("spDevelopment")
         self.sprint = kwargs.get("sprint")
         self.priority = kwargs.get("priority")
         self.updated_by = kwargs.get("updatedBy")
         self.updated_at = kwargs.get("updatedAt")
+
+    @property
+    def off_the_plan(self):
+        return bool(self._off_the_plan)
+
+    @property
+    def moving_next_sprint(self):
+        return bool(self._moving_on_to_the_next_sprint)
+
+    @property
+    def summary(self):
+        return self._summary.replace('"', '')
+
+    @property
+    def hyperlink(self) -> str:
+        return f'=HYPERLINK("{self.url()}"; "{self.name()}")'
 
     def url(self) -> str:
         return f"https://tracker.yandex.ru/{self.key}"
